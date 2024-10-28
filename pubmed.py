68bafea67837f8ac43000ec896686982adafa956
Nolan McCafferty <nolanmccafferty@gmail.com>
2024-10-28 16:03:46 -0500

    Multithread pubmed

--- pubmed.py
+++ pubmed.py
@@ -1,67 +1,120 @@
 import gzip
 import tempfile
 from ftplib import FTP
-
 import boto3
+from queue import Queue
+import threading
+import argparse
 
 from common import S3_BUCKET, LOGGER
 
 
-# FTP file functions
 def pubmed_ftp_client():
     ftp = FTP('ftp.ncbi.nlm.nih.gov')
     ftp.login()
     return ftp
 
+
 def retrieve_file(ftp_client, filename):
     local_filename = tempfile.mkstemp()[1]
-
     with open(local_filename, 'wb') as f:
         ftp_client.retrbinary(f'RETR {filename}', f.write)
-
     return local_filename
+
 
 def get_xml_content(xml_gz_filename):
     with gzip.open(xml_gz_filename, "rb") as xml_file:
         return xml_file.read()
 
+
 def get_existing_fnames(s3):
     existing_fnames = set()
     obj_list = s3.list_objects_v2(Bucket=S3_BUCKET,
-                                   Prefix='pubmed',
-                                   MaxKeys=1000)
+                                  Prefix='pubmed',
+                                  MaxKeys=1000)
     for o in obj_list["Contents"]:
         existing_fnames.add(o['Key'])
     if 'NextToken' not in obj_list or not obj_list['NextToken']:
         return existing_fnames
     while True:
         obj_list = s3.list_objects_v2(Bucket=S3_BUCKET,
-                                   Prefix='pubmed',
-                                   MaxKeys=1000,
-                                   ContinuationToken=obj_list['NextToken'])
+                                      Prefix='pubmed',
+                                      MaxKeys=1000,
+                                      ContinuationToken=obj_list['NextToken'])
         for o in obj_list["Contents"]:
             existing_fnames.add(o['Key'])
         if 'NextToken' not in obj_list or not obj_list['NextToken']:
             break
     return existing_fnames
 
-def main():
+
+def download_worker(q, total_files):
+    s3 = boto3.client('s3')
+    while True:
+        item = q.get()
+        if item is None:
+            break
+
+        i, filename = item
+        try:
+            LOGGER.info(f'Fetching {filename} ({i + 1}/{total_files})')
+            ftp = pubmed_ftp_client()
+            temp_fname = retrieve_file(ftp, filename)
+            s3.upload_file(temp_fname, S3_BUCKET, 'pubmed/' + filename)
+            ftp.quit()
+            LOGGER.info(f'Finished {filename} ({i + 1}/{total_files})')
+        except Exception as e:
+            LOGGER.error(f"Error processing {filename}: {e}")
+
+        q.task_done()
+
+
+def main(num_threads):
     s3 = boto3.client('s3')
     existing_fnames = get_existing_fnames(s3)
+
     ftp = pubmed_ftp_client()
     ftp.cwd('/pubmed/baseline')
-    baseline_fnames = [f'/pubmed/baseline/{f}' for f in ftp.nlst() if f.endswith('.xml.gz')]
+    baseline_fnames = [f'/pubmed/baseline/{f}' for f in ftp.nlst() if
+                       f.endswith('.xml.gz')]
     ftp.cwd('/pubmed/updatefiles')
-    update_fnames = [f'/pubmed/updatefiles/{f}' for f in ftp.nlst() if f.endswith('.xml.gz')]
+    update_fnames = [f'/pubmed/updatefiles/{f}' for f in ftp.nlst() if
+                     f.endswith('.xml.gz')]
     remote_filenames = sorted(baseline_fnames + update_fnames)
     ftp.quit()
-    for i, filename in enumerate([f for f in remote_filenames if f not in existing_fnames]):
-        LOGGER.info(f'Fetching {filename} ({i + 1}/{len(remote_filenames)})')
-        ftp = pubmed_ftp_client()
-        temp_fname = retrieve_file(ftp, filename)
-        s3.upload_file(temp_fname, S3_BUCKET, 'pubmed/' + filename)
-        ftp.quit()
-        LOGGER.info(f'Finished {filename} ({i + 1}/{len(remote_filenames)})')
+
+    files_to_process = [f for f in remote_filenames if f not in existing_fnames]
+    total_files = len(files_to_process)
+
+    if not files_to_process:
+        LOGGER.info("No new files to process")
+        return
+
+    download_queue = Queue()
+    workers = []
+
+    for _ in range(num_threads):
+        t = threading.Thread(target=download_worker,
+                             args=(download_queue, total_files))
+        t.start()
+        workers.append(t)
+
+    for i, filename in enumerate(files_to_process):
+        download_queue.put((i, filename))
+
+    for _ in workers:
+        download_queue.put(None)
+
+    for w in workers:
+        w.join()
+
+    LOGGER.info(f"Completed processing {total_files} files")
+
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--threads', type=int, default=20,
+                        help='Number of download threads')
+    args = parser.parse_args()
+
+    main(args.threads)
